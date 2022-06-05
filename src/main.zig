e3c69236aa0ca2235d61bdf41ae5401d909cea7a
r00ster91 <r00ster91@proton.me>
2022-06-05 23:33:47 +0200

    feat(helpers): add `sprinkleSlice`

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
+const mem = std.mem;
 const debug = std.debug;
+const testing = std.testing;
 
 const Bot = @import("Bot.zig");
 
@@ -47,5 +49,36 @@ test {
     _ = @import("matrix.zig");
     _ = @import("Bot/CommandInput.zig");
     _ = @import("Bot/commands/Help.zig");
-    _ = @import("Bot/commands/TypingTest.zig");
+}
+
+/// Inserts `sprinkle` into `sprinklee` at every position
+/// so that e.g. `sprinkleSlice(..., u8, "abc", " ")` is "a b c".
+fn sprinkleSlice(allocator: mem.Allocator, comptime T: type, sprinklee: []const T, sprinkle: []const T) ![]const T {
+    var sprinkled = try allocator.alloc(T, sprinklee.len + (sprinklee.len - 1) * sprinkle.len);
+    var index: usize = 0;
+    while (index < sprinklee.len) : (index += 1) {
+        const offset = sprinkle.len + 1;
+        sprinkled[index * offset] = sprinklee[index];
+
+        if (index == sprinklee.len - 1)
+            break;
+        var sprinkle_index: usize = 0;
+        while (sprinkle_index < sprinkle.len) : (sprinkle_index += 1)
+            sprinkled[index * offset + (sprinkle_index + 1)] = sprinkle[sprinkle_index];
+    }
+    return sprinkled;
+}
+
+test "sprinkling slices" {
+    var sprinkled = try sprinkleSlice(testing.allocator, u8, "hello world", " ");
+    try testing.expectEqualSlices(u8, "h e l l o   w o r l d", sprinkled);
+    testing.allocator.free(sprinkled);
+
+    sprinkled = try sprinkleSlice(testing.allocator, u8, "hello", "__");
+    try testing.expectEqualSlices(u8, "h__e__l__l__o", sprinkled);
+    testing.allocator.free(sprinkled);
+
+    sprinkled = try sprinkleSlice(testing.allocator, u8, "abc", " ");
+    try testing.expectEqualSlices(u8, "a b c", sprinkled);
+    testing.allocator.free(sprinkled);
 }
