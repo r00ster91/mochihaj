331c06a06113ff5a536cb0845660f3a5aea86637
r00ster91 <r00ster91@proton.me>
2022-06-05 23:31:29 +0200

    feat(http): parallel HTTP request handling
    This makes use of libcurl's multi interface without Zig's `async`-related features to process HTTP requests in parallel.

--- src/http.zig
+++ src/http.zig
@@ -20,8 +20,16 @@ pub const Response = struct {
     body: []const u8,
 };
 
+/// An interface to allow making HTTP requests to HTTP servers.
+///
+/// This supports making HTTP requests in parallel.
+/// It's recommended to collect HTTP requests in batches as much as possible
+/// until they're actually required to be done, so that they can be done in parallel.
 pub const Client = struct {
-    curl_handle: *curl.CURL,
+    primary_curl_easy_handle: *curl.CURL,
+    curl_multi_handle: *curl.CURLM,
+    // TODO: later use some kind of pool to reuse handles?
+    curl_easy_handles: std.ArrayList(*curl.CURL),
     curl_headers: *curl.curl_slist,
     hostname: []const u8,
     encryption: Encryption,
@@ -34,22 +42,6 @@ pub const Client = struct {
         if (curl.curl_global_init(curl.CURL_GLOBAL_SSL) != curl.CURLE_OK)
             return error.InitializationFailed;
 
-        const curl_handle = curl.curl_easy_init() orelse return error.InitializationFailed;
-
-        // We want TLS 1.3 and HTTP/2
-        if (curl.curl_easy_setopt(
-            curl_handle,
-            curl.CURLOPT_SSLVERSION,
-            curl.CURL_SSLVERSION_TLSv1_3,
-        ) != curl.CURLE_OK)
-            unreachable;
-        if (curl.curl_easy_setopt(
-            curl_handle,
-            curl.CURLOPT_HTTP_VERSION,
-            curl.CURL_HTTP_VERSION_2_PRIOR_KNOWLEDGE,
-        ) != curl.CURLE_OK)
-            unreachable;
-
         const curl_headers = curl_headers: {
             // We don't need Accept
             var curl_headers: [*c]curl.curl_slist = @as(
@@ -57,20 +49,51 @@ pub const Client = struct {
                 curl.curl_slist_append(null, "Accept:"),
             ) orelse return error.FailedSettingHeader;
 
-            // This only needs to be set once
-            if (curl.curl_easy_setopt(curl_handle, curl.CURLOPT_HTTPHEADER, curl_headers) != curl.CURLE_OK)
-                unreachable;
-
             break :curl_headers curl_headers;
         };
 
-        const client = Client{
-            .curl_handle = curl_handle,
+        var client = Client{
+            .primary_curl_easy_handle = undefined,
+            .curl_multi_handle = curl.curl_multi_init().?,
+            .curl_easy_handles = std.ArrayList(*curl.CURL).init(allocator),
             .curl_headers = curl_headers,
             .hostname = hostname,
             .encryption = encryption,
             .body = std.ArrayList(u8).init(allocator),
         };
+        client.primary_curl_easy_handle = try client.getCurlEasyHandle();
+
+        // // Setup the error string
+        // if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_ERRORBUFFER, &client.error_string) != curl.CURLE_OK)
+        //     unreachable;
+
+        // TODO: remove this
+        // if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_VERBOSE, @as(c_int,1)) != curl.CURLE_OK)
+        //     unreachable;
+
+        return client;
+    }
+
+    fn getCurlEasyHandle(self: Client) !*curl.CURL {
+        const curl_easy_handle = curl.curl_easy_init() orelse return error.InitializationFailed;
+
+        // We want TLS 1.3 and HTTP/2
+        if (curl.curl_easy_setopt(
+            curl_easy_handle,
+            curl.CURLOPT_SSLVERSION,
+            curl.CURL_SSLVERSION_TLSv1_3,
+        ) != curl.CURLE_OK)
+            unreachable;
+        if (curl.curl_easy_setopt(
+            curl_easy_handle,
+            curl.CURLOPT_HTTP_VERSION,
+            curl.CURL_HTTP_VERSION_2_PRIOR_KNOWLEDGE,
+        ) != curl.CURLE_OK)
+            unreachable;
+
+        // This only needs to be set once
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_HTTPHEADER, self.curl_headers) != curl.CURLE_OK)
+            unreachable;
 
         // This callback lets us read the body's content in chunks.
         // The amount of data we get in one chunk could be any amount
@@ -93,28 +116,31 @@ pub const Client = struct {
                 return nmemb;
             }
         }.write_callback;
-        if (curl.curl_easy_setopt(curl_handle, curl.CURLOPT_WRITEFUNCTION, write_callback) != curl.CURLE_OK)
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_WRITEFUNCTION, write_callback) != curl.CURLE_OK)
             unreachable;
 
         // Setup the error string
-        if (curl.curl_easy_setopt(curl_handle, curl.CURLOPT_ERRORBUFFER, &client.error_string) != curl.CURLE_OK)
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_ERRORBUFFER, &self.error_string) != curl.CURLE_OK)
             unreachable;
 
-        // TODO: remove this
-        // if (curl.curl_easy_setopt(curl_handle, curl.CURLOPT_VERBOSE, @as(c_int,1)) != curl.CURLE_OK)
-        //     unreachable;
-
-        return client;
+        return curl_easy_handle;
     }
 
     pub fn deinit(self: Client) void {
-        curl.curl_global_cleanup();
-        curl.curl_easy_cleanup(self.curl_handle);
+        for (self.curl_easy_handles.items) |curl_easy_handle| {
+            if (curl.curl_multi_remove_handle(self.curl_multi_handle, curl_easy_handle) != curl.CURLE_OK)
+                unreachable;
+            curl.curl_easy_cleanup(curl_easy_handle);
+        }
+        self.curl_easy_handles.deinit();
+        if (curl.curl_multi_cleanup(self.curl_multi_handle) != curl.CURLE_OK)
+            unreachable;
         curl.curl_slist_free_all(self.curl_headers);
+        curl.curl_global_cleanup();
         self.body.deinit();
     }
 
-    /// Sets a request header. `key` is case-insensitive.
+    /// Sets a request header for all following requests. `key` is case-insensitive.
     ///
     /// Use `null` for `value` to remove the header.
     pub fn setHeader(self: *Client, allocator: mem.Allocator, key: []const u8, value: ?[]const u8) !void {
@@ -129,18 +155,21 @@ pub const Client = struct {
         ) orelse return error.FailedSettingHeader;
     }
 
-    pub fn request(self: *Client, allocator: mem.Allocator, method: std.http.Method, path: []const u8, body: ?[]const u8) !Response {
+    /// Adds a request to the queue to be batched and returns a request index to it.
+    pub fn enqueueRequest(self: *Client, allocator: mem.Allocator, method: std.http.Method, path: []const u8, body: ?[]const u8) !usize { //usize { // !Response {
+        const curl_easy_handle = try self.getCurlEasyHandle();
+
         if (body) |body_value| {
             if (@import("builtin").mode == .Debug)
                 if (!method.requestHasBody())
                     std.debug.panic("{s} request must not have body", .{@tagName(method)});
-            if (curl.curl_easy_setopt(self.curl_handle, curl.CURLOPT_POSTFIELDS, body_value.ptr) != curl.CURLE_OK)
+            if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_POSTFIELDS, body_value.ptr) != curl.CURLE_OK)
                 unreachable;
-            if (curl.curl_easy_setopt(self.curl_handle, curl.CURLOPT_POSTFIELDSIZE, body_value.len) != curl.CURLE_OK)
+            if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_POSTFIELDSIZE, body_value.len) != curl.CURLE_OK)
                 unreachable;
         }
 
-        if (curl.curl_easy_setopt(self.curl_handle, curl.CURLOPT_CUSTOMREQUEST, @tagName(method).ptr) != curl.CURLE_OK)
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_CUSTOMREQUEST, @tagName(method).ptr) != curl.CURLE_OK)
             return error.OutOfMemory;
 
         const scheme = switch (self.encryption) {
@@ -150,46 +179,137 @@ pub const Client = struct {
         const url = try fmt.allocPrintZ(allocator, "{s}://{s}{s}", .{ scheme, self.hostname, path });
         defer allocator.free(url);
 
-        if (curl.curl_easy_setopt(self.curl_handle, curl.CURLOPT_URL, url.ptr, curl.CURLU_PATH_AS_IS) != curl.CURLE_OK)
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_URL, url.ptr, curl.CURLU_PATH_AS_IS) != curl.CURLE_OK)
             return error.SettingURLFailed;
 
         self.body.items.len = 0;
 
-        if (curl.curl_easy_setopt(self.curl_handle, curl.CURLOPT_WRITEDATA, &self.body) != curl.CURLE_OK)
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_WRITEDATA, &self.body) != curl.CURLE_OK)
             unreachable;
 
-        log.info("sending {s} request to {s}", .{ @tagName(method), url });
-        log.debug("request body: {s}", .{ body });
+        log.info("adding to queue: {s} request to {s}", .{ @tagName(method), url });
+        if (body) |body_value| log.debug("request body: {s}", .{body_value});
 
-        // Here `write_callback` will be invoked
-        const result = curl.curl_easy_perform(self.curl_handle);
+        if (curl.curl_multi_add_handle(self.curl_multi_handle, curl_easy_handle) != curl.CURLE_OK)
+            return error.AddingCurlEasyHandleFailed;
+
+        const request_index = self.curl_easy_handles.items.len;
+        try self.curl_easy_handles.append(curl_easy_handle);
+        return request_index;
+    }
+
+    fn awaitRequest(self: *Client, allocator: mem.Allocator, method: std.http.Method, path: []const u8, body: ?[]const u8) !Response {
+        const curl_easy_handle = try self.getCurlEasyHandle();
+
+        if (body) |body_value| {
+            if (@import("builtin").mode == .Debug)
+                if (!method.requestHasBody())
+                    std.debug.panic("{s} request must not have body", .{@tagName(method)});
+            if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_POSTFIELDS, body_value.ptr) != curl.CURLE_OK)
+                unreachable;
+            if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_POSTFIELDSIZE, body_value.len) != curl.CURLE_OK)
+                unreachable;
+        }
+
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_CUSTOMREQUEST, @tagName(method).ptr) != curl.CURLE_OK)
+            return error.OutOfMemory;
+
+        const scheme = switch (self.encryption) {
+            .unencrypted => "http",
+            .encrypted => "https",
+        };
+        const url = try fmt.allocPrintZ(allocator, "{s}://{s}{s}", .{ scheme, self.hostname, path });
+        defer allocator.free(url);
+
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_URL, url.ptr, curl.CURLU_PATH_AS_IS) != curl.CURLE_OK)
+            return error.SettingURLFailed;
+
+        if (curl.curl_easy_setopt(curl_easy_handle, curl.CURLOPT_WRITEDATA, &self.body) != curl.CURLE_OK)
+            unreachable;
+
+        log.info("adding to queue: {s} request to {s}", .{ @tagName(method), url });
+        if (body) |body_value| log.debug("request body: {s}", .{body_value});
+
+        // This will invoke `write_callback`.
+        const result = curl.curl_easy_perform(curl_easy_handle);
         if (result != curl.CURLE_OK) {
             log.err("{}: {s}", .{ result, self.error_string });
             return error.RequestFailed;
         }
-
         var status: std.http.Status = undefined;
-        if (curl.curl_easy_getinfo(self.curl_handle, curl.CURLINFO_RESPONSE_CODE, &status) != curl.CURLE_OK)
+        if (curl.curl_easy_getinfo(curl_easy_handle, curl.CURLINFO_RESPONSE_CODE, &status) != curl.CURLE_OK)
             unreachable;
         if (@enumToInt(status) == 0)
-            return error.NoResponseCode;
+            // The request succeeded so `status` is guaranteed to be non-zero
+            unreachable;
 
         log.debug("response status: {}", .{status});
         log.debug("response body: {s}", .{self.body.items});
         return Response{ .status = status, .body = self.body.items };
     }
+
+    /// Returns a response belonging to a request index, or `null` if the request hasn't been processed.
+    pub fn getResponse(self: Client, request_index: usize) ?Response {
+        var status: std.http.Status = undefined;
+        if (curl.curl_easy_getinfo(self.curl_easy_handles.items[request_index], curl.CURLINFO_RESPONSE_CODE, &status) != curl.CURLE_OK)
+            unreachable;
+        if (@enumToInt(status) == 0)
+            return null;
+        log.debug("response status: {}", .{status});
+        log.debug("response body: {s}", .{self.body.items});
+        return Response{ .status = status, .body = self.body.items };
+    }
+
+    pub fn awaitAllRequests(self: Client) void {
+        var polls: usize = 0;
+
+        var transfers_running: c_int = undefined;
+        var mc: curl.CURLMcode = curl.curl_multi_perform(self.curl_multi_handle, &transfers_running);
+        while (transfers_running > 0) {
+            mc = curl.curl_multi_perform(self.curl_multi_handle, &transfers_running);
+
+            if (transfers_running > 0) {
+                mc = curl.curl_multi_poll(self.curl_multi_handle, null, 0, 1000, null);
+            }
+
+            polls += 1;
+
+            if (mc != 0)
+                break;
+        }
+
+        log.debug("all transfers finished in {d} polls", .{polls});
+    }
 };
 
-test "GET requests" {
-    const hostname = "www.google.com";
+const test_hostname = "www.google.com";
 
-    var client = try Client.init(testing.allocator, hostname, .encrypted);
+test "parallel GET requests" {
+    var client = try Client.init(testing.allocator, test_hostname, .encrypted);
     defer client.deinit();
-    var response = try client.request(testing.allocator, .GET, "/", null);
-    try testing.expect(response.status == .ok);
-    try testing.expect(response.body.len != 0);
 
-    response = try client.request(testing.allocator, .POST, "/", "hello");
+    var request_indices = [_]usize{
+        try client.enqueueRequest(testing.allocator, .GET, "/", null),
+        try client.enqueueRequest(testing.allocator, .GET, "/", null),
+        try client.enqueueRequest(testing.allocator, .GET, "/", null),
+        try client.enqueueRequest(testing.allocator, .GET, "/", null),
+        try client.enqueueRequest(testing.allocator, .GET, "/", null),
+    };
+
+    client.awaitAllRequests();
+
+    for (request_indices) |request_index| {
+        const response = client.getResponse(request_index).?;
+        try testing.expect(response.status == .ok);
+        try testing.expect(response.body.len != 0);
+    }
+}
+
+test "POST requests" {
+    var client = try Client.init(testing.allocator, test_hostname, .encrypted);
+    defer client.deinit();
+
+    const response = try client.awaitRequest(testing.allocator, .POST, "/", "hello");
     try testing.expect(response.status == .method_not_allowed);
     try testing.expect(response.body.len != 0);
 }
